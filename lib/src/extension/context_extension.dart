0cb813907aed321438d84aa32e0abf1d2b832707
ismailyegnr <ismailyeginer@gmail.com>
2026-03-11 10:29:46 +0100

    chore: delete unused gets

--- lib/src/extension/context_extension.dart
+++ lib/src/extension/context_extension.dart
@@ -6,13 +6,7 @@ extension ContextExtension on BuildContext {
   ExpandableThemeData get expandableTheme => ExpandableTheme.of(this);
 
   double get height => MediaQuery.of(this).size.height;
-  double get width => MediaQuery.of(this).size.width;
 
   double get lowValue => height * 0.01;
   double get normalValue => height * 0.02;
-  double get mediumValue => height * 0.04;
-  double get highValue => height * 0.1;
-
-  double dynamicWidth(double val) => width * val;
-  double dynamicHeight(double val) => height * val;
 }
